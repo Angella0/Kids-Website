146e713e56272caee415cbedaaa6711c2957ce74
Angella0 <angellasimbwa@gmail.com>
2021-09-13 14:25:05 +0300

    commit

--- java.js
+++ java.js
@@ -1,35 +1,10 @@
 
-// const shopping ={
-//     Hand_made_heirlooms:35000,
-//     organic_baby_clothes:3500
-// };
 
-// const shoppingList = (resolve, reject) => {
-//     if (shopping.Hand_made_heirlooms >= 35000 ){
-//         resolve ("Product ready to be sold");
-//     }else{
-//         reject("Sorry, the product has been sold out");
-//     }
-// };
+(function(){
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
 
-// const AddToCart = () => {
-//     return new Promise(shoppingList);
-// }
-// const ShoppingNew = AddToCart();
-// console.log(ShoppingNew);
-function product(){
-
-let AddToCart;
-for ( AddToCart == 0;AddToCart>=35000,AddToCart ++;)
-
-if(AddToCart >= 35000){
-    console.log("The product is ready to be sold")
-}else{
-    console.log("Sorry the product has been sold out")
-}
-  document.getElementById("gonext").innerHTML= "product"
-
- }
-
-
-// alert('Hello, welcome to our website. Kindly explore more');
+    cartInfo.addEventListener('click', function(){
+      cart.classList.toggle('show-cart')  
+    })
+})();
